34b0f214b6cf2fd63f5c75ad0d50f5e4db6905b6
Ashish Saini <47918474+ashus3868@users.noreply.github.com>
2024-07-06 10:18:23 +0530

    Soil Moisture Sensor ADC values to Firebase Realtime database

---
+++ main/firebase_config.h
@@ -0,0 +1,13 @@
+// Wifi Credentials
+#define SSID "EXAMPLE_SSID"
+#define PASSWORD "EXAMPLE_PASSWORD"
+
+// Read readme.md to properly configure api key and authentication
+
+// create a new api key and add it here 
+#define API_KEY "YOUR-API-KEY-HERE"
+// Copy your firebase real time database link here 
+#define DATABASE_URL "https://[PROJECT-NAME-HERE].firebaseio.com/"  
+
+#define USER_EMAIL "USERNAME"   // This gmail does not exist outside your database. it only exists in the firebase project as a user
+#define USER_PASSWORD "PASSWORD"      // Dont add your gmail credentials. Setup users authentication in your Firebase project first
